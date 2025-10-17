156f81fc81d3b991b28589ef55301fc2417c839a
maverick-enigma-ai <mq@ingrowthmode.com>
2025-10-17 20:15:07 +0100

    More changes to get over strict TS issues

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { ChatInterface } from './components/ChatInterface';
-import DebriefingScreen  from './components/DebriefingScreen';
-import DashboardScreen from './components/DashboardScreen';
+import { DebriefingScreen }  from './components/DebriefingScreen';
+import { DashboardScreen } from './components/DashboardScreen';
 import { HomeScreen } from './components/HomeScreen';
 import { ProfileScreen } from './components/ProfileScreen';
 import { HistoryScreen } from './components/HistoryScreen';
